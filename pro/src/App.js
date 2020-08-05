aa98c2fad79ebe0eeafb9dfc8a992942c26edc3c
VonArzu <Von.Arzu@rutgers.edu>
2020-08-05 12:13:53 -0400

    Components Initialized

---
+++ pro/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "./Home";
+import "./App.css";
+import axios from "axios";
+import Daily from "./Daily";
+import { Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    
+  );
+}
+
+export default App;
---
+++ pro/src/Home.js
@@ -0,0 +1,12 @@
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import ToDo from "./ToDo"
+import { Route, Link } from "react-router-dom";
+
+function Home() {
+  return (
+    
+  );
+}
+
+export default Home;
